660a1119bcd6f67d651bb1b7809d5b4a1cc384f0
Élisson Barbosa <barbosaelisson6@gmail.com>
2024-09-20 00:55:01 -0300

    Criação do projeto

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html =
+    document.documentElement /* document.documentElement serve para manipular todo o HTML */
+  html.classList.toggle("light")
+
+  //Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    //Se tiver Light mode, Adiciona a imagem nova
+    img.setAttribute("src", "./assets/Avatar-1.png")
+  } else {
+    //Se não tiver light mode, manter a imagem
+    img.setAttribute("src", "/assets/Avatar-2.png")
+  }
+}
+
+//Se tiver Light mode, Adiciona a imagem nova
+//
+//  html.classList.remove("light")
+//
+//   html.classList.add("light")
